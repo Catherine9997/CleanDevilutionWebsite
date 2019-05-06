fb8782d099b5a7871cec7c292f4f730833b071ad
Qndel <stefan551@o2.pl>
2019-05-06 23:30:47 +0200

    AJenbo makes it more secure :D

--- updateFile.php
+++ updateFile.php
@@ -1,33 +1,68 @@
 <?php
-// returns true if $needle is a substring of $haystack
-function contains($needle, $haystack)
+/**
+ * Returns true if $needle is a substring of $haystack.
+ */
+function contains(string $needle, string $haystack): bool
 {
-    return strpos($haystack, $needle) !== false;
+    return mb_strpos($haystack, $needle) !== false;
 }
 
-if(isset($_POST["repo"]) and ctype_digit($_POST["repo"]) and isset($_POST["fileName"]) and isset($_POST["fileContent"]) and isset($_POST["functionName"])){
-	$myfile = fopen("devilution_".$_POST["repo"]."/Source/".$_POST["fileName"], "w") or die("Unable to open file!");
-	fwrite($myfile, $_POST["fileContent"]);
-	fclose($myfile);	
-	$out = "";
-	$mvse = array();
-	$hideaddr = "";
-	if(isset($_POST["hideaddr"])){
-		$hideaddr = "--no-mem-disp";
-	}
-	$out = exec("compare.bat ".$_POST["repo"]." ".$_POST["functionName"]. " ".$hideaddr, $mvse, $out);
-	$skipped = 0;
-	foreach( $mvse as $line ) {
-		if($line != ""){
-			if(contains($_POST["fileName"],$line) or contains("Found ".$_POST["functionName"],$line) or contains(" error ",$line) or contains("Could not find the specified symbol in the config",$line)){
-				if($skipped < 2){
-					$skipped++;
-				} else{
-					echo json_encode(htmlspecialchars( $line ));
-				}
-			}
-		}
-	}
-	//echo json_encode($out);
+/**
+ * Look for scarry input and return true if found.
+ */
+function isScarry(string $haystack): bool
+{
+    return preg_match('/[^a-z0-9_-]/ui');
 }
-?>
+
+if (!isset($_POST['repo'], $_POST['fileName'], $_POST['fileContent'], $_POST['functionName'])) {
+    die('No input!');
+}
+
+if (!ctype_digit($_POST['repo'])
+    || isScarry($_POST['fileName'])
+    || isScarry($_POST['functionName'])
+) {
+    die('Invalid input!');
+}
+
+$filePath = 'devilution_' . $_POST['repo'] . '/Source/' . $_POST['fileName'];
+
+$myfile = fopen($filePath, 'w');
+if (!$myfile) {
+    die('Unable to open file!');
+}
+
+fwrite($myfile, $_POST['fileContent']);
+fclose($myfile);
+$out      = '';
+$mvse     = [];
+$hideaddr = '';
+if (array_key_exists('hideaddr', $_POST)) {
+    $hideaddr = '--no-mem-disp';
+}
+
+$out = exec('compare.bat ' . $_POST['repo'] . ' ' . $_POST['functionName'] . ' ' . $hideaddr, $mvse, $out);
+
+$skipped = 0;
+foreach ($mvse as $line) {
+    if ($line === '') {
+        continue;
+    }
+
+    if (!contains($_POST['fileName'], $line)
+        && !contains('Found ' . $_POST['functionName'], $line)
+        && !contains(' error ', $line)
+        && !contains('Could not find the specified symbol in the config', $line)
+    ) {
+        continue;
+    }
+
+    if ($skipped < 2) {
+        $skipped++;
+        continue;
+    }
+
+    echo json_encode(htmlspecialchars($line));
+    break;
+}
